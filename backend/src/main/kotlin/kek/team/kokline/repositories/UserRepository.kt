7bb9a6d03a86568901b862e26927b7dc7454076a
o.kedrovskikh <o.kedrovskikh@tinkoff.ru>
2023-09-23 00:23:18 +0500

    Add exposed integration

--- backend/src/main/kotlin/kek/team/kokline/repositories/UserRepository.kt
+++ backend/src/main/kotlin/kek/team/kokline/repositories/UserRepository.kt
@@ -1,13 +1,34 @@
 package kek.team.kokline.repositories
 
+import kek.team.kokline.entities.UserEntity
+import kek.team.kokline.factories.dbQuery
 import kek.team.kokline.models.User
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
 
-class UserRepository(private val storage: MutableList<User> = mutableListOf()) {
-    fun createUser(user: User) = storage.add(user)
+class UserRepository {
 
-    fun findAll() = storage.toList()
+    suspend fun create(user: User): User {
+        val statement = dbQuery { UserEntity.insert { it[nickname] = user.nickname } }
+        return statement.resultedValues?.singleOrNull()?.let(::resultRowToModel) ?: error("Cannot create user")
+    }
 
-    fun findUser(id: Long?) = storage.filter { it.id != null }.find { it.id == id }
+    suspend fun findAll(): List<User> = dbQuery { UserEntity.selectAll().map(::resultRowToModel) }
 
-    fun delete(id: Long?) = storage.removeIf { it.id == id }
+    suspend fun findById(id: Long): User? = dbQuery {
+        UserEntity.select { UserEntity.id eq id }.map(::resultRowToModel).singleOrNull()
+    }
+
+    suspend fun edit(user: User): Boolean = dbQuery {
+        UserEntity.update({ UserEntity.id eq requireNotNull(user.id) }) { it[nickname] = user.nickname } > 0
+    }
+
+    suspend fun deleteById(id: Long): Boolean = dbQuery { UserEntity.deleteWhere { UserEntity.id eq id } > 0 }
+
+    private fun resultRowToModel(row: ResultRow): User = User(row[UserEntity.id], row[UserEntity.nickname])
 }
